581050ee4008f2fcfb35aa90be5b4cf1cbdb976c
Todd DeLuca <no-spam@todddeluca.com>
2011-11-12 11:42:08 -0500

    Ignore html_video_test dir when deploying

--- fabfile.py
+++ fabfile.py
@@ -115,7 +115,8 @@ def deploy():
 
     # copy files to remote destintation, excluding backups, .svn dirs, etc.
     # do not deploy these files/dirs.
-    deployExcludes = ['**/old', '**/old/**', '**/semantic.cache', '**/.svn', '**/.svn/**', '**/*.pyc', '**/*.pyo', '**/.DS_Store', '**/*~']
+    deployExcludes = ['**/old', '**/old/**', '**/semantic.cache', '**/.svn', '**/.svn/**', '**/*.pyc',
+                      '**/*.pyo', '**/.DS_Store', '**/*~', 'html_video_test/']
     filterArgs = []
     for e in deployExcludes:
         filterArgs += ['-f', '- {}'.format(e)]
